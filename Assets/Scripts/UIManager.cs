a9e44054de7c76f3cdf29c0f694e7a2106d2f105
Hae1385 <206494390+Hae1385@users.noreply.github.com>
2025-06-21 17:33:18 +0900

    🎉 add : UI매니저 추가 및 화면 가로고정

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //화면 가로고정
+        Screen.orientation = ScreenOrientation.AutoRotation;
+        Screen.autorotateToLandscapeLeft = true;
+        Screen.autorotateToLandscapeRight = true;
+        Screen.autorotateToPortrait = false;
+        Screen.autorotateToPortraitUpsideDown = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
