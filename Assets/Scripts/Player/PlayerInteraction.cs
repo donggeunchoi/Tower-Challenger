2e70512ecb2c5ebd26928127855e1768345d5625
Hae1385 <206494390+Hae1385@users.noreply.github.com>
2025-06-21 20:53:34 +0900

    🍻 test : 테스트용 인터렉션
    테스트용으로 인터렉션이 활성화 되면 버튼이 보이도록했습니다
    
    추후 수정 사항 버튼은 항상보이고
    인터렉션이 활성화되면 버튼 이미지가 변경되도록 할 예정
    비활성화 상태에서 return;
    활성화 상태에선 해당 인터렉션이 가지고 있는 프리팹 UI활성화

---
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public Button button;
+
+    private void Start()
+    {
+        button.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Interaction"))
+        {
+            button.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Interaction"))
+        {
+            button.gameObject.SetActive(false);
+        }
+    }
+}
