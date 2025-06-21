b5e4e27099c2f56eecb4c2d5ed05e372166bde00
ChaWooJin <chawoojin06@gmail.com>
2025-06-22 08:42:23 +0900

    update : PlayerMove
    다이노 런 점프 로직

---
+++ Assets/Scripts/Dino_Button_UI.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Dino_Button_UI : MonoBehaviour
+{
+    public Dino_Player_Move player;
+
+    public void DinoRunButton()
+    {
+        player.PlayerJump();
+    }
+}
