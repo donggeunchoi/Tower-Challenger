95462d440e497ae478414413b47dca037da80b6f
ChaWooJin <chawoojin06@gmail.com>
2025-06-22 09:08:00 +0900

    update : 폴더 생성 , fix : 스크립트 이름 변경
    폴더 생성 : 스크립트 폴더안에 다이노런 스크립트 폴더 생성
    
    스크립트 이름 변경 :

---
+++ Assets/Scripts/DinoScripts/DIno_Map_Move.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class DIno_Map_Move : MonoBehaviour
+{
+    
+}
---
+++ Assets/Scripts/DinoScripts/Dino_UI_Button.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Dino_UI_Button : MonoBehaviour
+{
+    public Dino_Player_Move player;
+
+    public void DinoRunButton()
+    {
+        player.PlayerJump();
+    }
+    
+    public void DionSlideButton()
+    {
+
+    }
+}
