016e21deaaf22f1a5de22fcd87a824ce20c1a3da
donggeunchoi <cdg01063149045@gmail.com>
2025-06-23 12:51:18 +0900

    Update
    뒷배경 루프돌림요

---
+++ Assets/Scripts/DinoRun_Donggeun/BackGroundLooper.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class BackGroundLooper : MonoBehaviour
+{
+    public float scrollSpeed = 5f;
+    public float backgroundWidth = 20f; // 배경 하나의 가로 길이
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.left * scrollSpeed * Time.deltaTime);
+
+        // 배경이 왼쪽으로 완전히 나가면 오른쪽으로 재배치
+        if (transform.position.x < -backgroundWidth)
+        {
+            transform.position += new Vector3(backgroundWidth * 2f, 0f, 0f);
+        }
+    }
+}
