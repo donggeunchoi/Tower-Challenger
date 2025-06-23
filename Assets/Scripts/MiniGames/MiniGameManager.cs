19bf333fe881d784e89f8baf71893fa6a427c50a
Hae1385 <206494390+Hae1385@users.noreply.github.com>
2025-06-23 11:27:12 +0900

    Fix, Add : 프로젝트 세팅 변경 및 게임매니저 추가

---
+++ Assets/Scripts/MiniGames/MiniGameManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class MiniGameManager : MonoBehaviour
+{
+    MiniGameManager miniGameManager;
+    public const int DEFALT_LP = 4;
+    public int bonusLP = 0;
+    public int currentLP;
+    private int bestStage;
+    private int currentStage;
+
+    private void Awake()
+    {
+        if (miniGameManager == null)
+        {
+            miniGameManager = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void Start()
+    {
+        currentLP = DEFALT_LP + bonusLP;
+        currentStage = 0;
+    }
+
+    public void StageClear()
+    {
+        currentStage++;
+        //랜덤스테이지
+    }
+
+    public void LPdown()
+    {
+        currentLP--;
+        if (currentLP <= 0)
+        {
+            if (currentStage > bestStage)
+                bestStage = currentStage;
+            GameOver();
+        }
+    }
+
+    private void GameOver()
+    {
+        //값을 넘겨주고
+        Destroy(this);
+    }
+}
