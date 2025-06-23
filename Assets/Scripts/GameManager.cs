3ebbb19c4a71bca5db154648fca2082c1e241d3d
Hae1385 <206494390+Hae1385@users.noreply.github.com>
2025-06-23 21:44:24 +0900

    Fix : 복구

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,34 +1,15 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance;
-    
-
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-        DontDestroyOnLoad(this.gameObject);
-    }
-
-    private void Start()
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
         
     }
 
-    public void StageClear()
-    {
-
-    }
-
-    public void FailStage()
+    // Update is called once per frame
+    void Update()
     {
         
     }
