0043e2c1437963664f73e3a11b5413d404278879
ChaWooJin <chawoojin06@gmail.com>
2025-06-24 04:07:31 +0900

    updata

---
+++ Assets/Scripts/UpAndDown/UpAndDown.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class UpAndDown : MonoBehaviour
+{
+    public string NumGenration()
+    {
+        float num = Random.Range(0f, 101f);
+        UpAndDownManager.instance.randomNumber = num;
+
+        return UpAndDownManager.instance.randomNumber.ToString("N0");
+    }
+
+    public void Success()
+    {
+
+    }
+}
---
+++ Assets/Scripts/UpAndDown/UpAndDownUI.cs
@@ -0,0 +1,18 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class UpAndDownUI : MonoBehaviour
+{
+    public TextMeshProUGUI number;
+    public TMP_InputField numInput;
+
+    public void InitUi()
+    {
+        number.text = UpAndDownManager.instance.upAndDown.NumGenration();
+    }
+
+    public void AnswerButton()
+    {
+
+    }
+}
