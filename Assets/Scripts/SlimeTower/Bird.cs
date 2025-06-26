946fac1366dd761f4686f5e703070964bc27ff8b
donggeunchoi <cdg01063149045@gmail.com>
2025-06-26 21:09:06 +0900

    Update : 슬라임 콜라이더 충돌로 이동 && 방향 전환 구현

--- Assets/Scripts/SlimeTower/Bird.cs
+++ Assets/Scripts/SlimeTower/Bird.cs
@@ -5,24 +5,38 @@ using UnityEngine;
 public class Bird : MonoBehaviour
 {
     //새가 좌우로 움직이는 역할을 담당할꺼야
-   public GameObject bird;
-   public float birdSpeed = 5f;
-   public Transform target;
+   public float birdSpeed = 2f;
+   private bool isMove = true;
+   private Vector3 direction;
+   public SpriteRenderer BirdSprite;
 
 
    void Start()
    {
-       bird =  GameObject.Find("Bird");
+       direction = Vector3.right;
    }
    void Update()
    {
        BirdMove();
    }
 
-//새가 좌우로 움직이게 만들려면 일단 초기 위치를 잡아주고
+//새가 좌우로 움직이게 만들려면 일단 초기 방향을 잡아주고
 //어떤 속도로 이동할껀지 생각을 해보자.
    public void BirdMove()
    {
-       
+       if (isMove)
+       {
+           transform.Translate(direction * birdSpeed * Time.deltaTime);
+       }
+   }
+
+   
+   public void OnTriggerEnter2D(Collider2D other)
+   {
+       if (other.CompareTag("TurnZone"))
+       {
+           direction = -direction;
+           BirdSprite.flipX = direction.x < 0;
+       }
    }
 }
