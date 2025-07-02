68ebc928596825da69b00997001cfed7f5dda80f
Hae1385 <206494390+Hae1385@users.noreply.github.com>
2025-07-02 17:44:17 +0900

    refector : 플레이어 인풋 리팩토링

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using JetBrains.Annotations;
+using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,6 +9,14 @@ public class GameManager : MonoBehaviour
 
     public GameObject stageManagerPrefab;
     public GameObject uiManagerPrefab;
+
+    public const int MAX_STAMINA = 5;
+    public int mainStamina;
+    public const float STAMINA_TIME = 0.5f;
+
+    //1800초 30분 마다 1참
+    //게임이 꺼져도 차게할 방법.... 시작시간과 끝시간을 계산해서 채워준다..?
+    //강종하면 어떻게하지.. 특정행동이나 시간마다 자동 저장을 해준다
 
     private void Awake()
     {
@@ -38,5 +47,26 @@ public class GameManager : MonoBehaviour
             if (stageManagerPrefab != null)
                 Instantiate(stageManagerPrefab);
         }
+    }
+
+    private void Update()
+    {
+        if (Time.time >= 0.5f)
+        {
+            AddStamina();
+        }
+    }
+
+    public void AddStamina()
+    {
+        mainStamina = Mathf.Max(mainStamina + 1, MAX_STAMINA);
+    }
+
+    public void UseStamina()
+    {
+        if (mainStamina < 0)
+            return;
+
+        mainStamina = Mathf.Max(mainStamina - 1, 0);
     }
 }
