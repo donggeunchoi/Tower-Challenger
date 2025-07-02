633ceed81102dbccfaf7f07519e06c50dacba662
Hae1385 <206494390+Hae1385@users.noreply.github.com>
2025-07-02 20:28:57 +0900

    Add : 스테미나 UI 업데이트 추가

---
+++ Assets/Scripts/Village/StaminaUI.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class StaminaUI : MonoBehaviour
+{
+    private GameManager gameManager;
+    private GameObject[] staminaUI;
+    private int stamina;
+
+    private void Start()
+    {
+        gameManager = GameManager.Instance;
+    }
+
+    private void Update()
+    {
+        if (stamina != gameManager.mainStamina)
+        {
+            UpdateStaminaUI();
+        }
+    }
+
+    private void UpdateStaminaUI()
+    {
+        for (int i = 0; i <= staminaUI.Length; i++)
+        {
+            staminaUI[i].gameObject.SetActive(false);
+        }
+
+        for (int i = 0; i <= gameManager.mainStamina; i++)
+        {
+            staminaUI[i].gameObject.SetActive(true);
+        }
+
+        stamina = gameManager.mainStamina;
+    }
+}
