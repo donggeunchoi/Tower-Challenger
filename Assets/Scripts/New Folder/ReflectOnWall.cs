23f6b7cc465d3e51849e100106cdf30ec1299167
whomery <whomery12345@gmail.com>
2025-07-04 20:47:49 +0900

    Update :어려운 게임 에셋 추가 적 팅김 로직 추가 맵 추가

---
+++ Assets/Scripts/New Folder/ReflectOnWall.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ReflectOnWall : MonoBehaviour
+{
+   
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.CompareTag("Wall"))
+        {
+            Vector3 rot = transform.eulerAngles;
+            rot.y += 180f;
+            transform.rotation = Quaternion.Euler(rot);
+        }
+    }
+}
