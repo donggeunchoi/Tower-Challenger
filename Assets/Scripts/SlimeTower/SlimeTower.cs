5cdc326236e042989d0dc77c460a11e2bb79b8da
Hae1385 <206494390+Hae1385@users.noreply.github.com>
2025-07-07 01:36:56 +0900

    Add : 슬라임 타워 기능 추가
    쿨타임 기능 추가
    바로 떨어지도록 변경

--- Assets/Scripts/SlimeTower/SlimeTower.cs
+++ Assets/Scripts/SlimeTower/SlimeTower.cs
@@ -9,6 +9,12 @@ public class SlimeTower : MonoBehaviour
     public Transform towerRoot;     //타워 포지션
     public int clearGameCount;      //몇개 쌓으면 클리어인지
 
+    [Header("쿨타임")]
+    [SerializeField] private int maxStack = 3;  //슬라임을 던질 수 있는 최대 스택갯수
+    [SerializeField] private float coolDown = 2f; //스택이 차는 쿨타임
+    [SerializeField] private int currentStack; //현재 던질 수 있는 스택
+    [SerializeField] private float stackTimer; //쿨타임을 측정해주는 타이머
+
     private GameObject currentSlime; //현재슬라임
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -25,6 +31,9 @@ public class SlimeTower : MonoBehaviour
                 clearGameCount = data.clearGameCount;
             }
         }
+
+        currentStack = maxStack;
+        stackTimer = 0;
     }
 
     // Update is called once per frame
@@ -33,20 +42,33 @@ public class SlimeTower : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             MoveSlime();
-            Invoke("SpawnSlime",0.5f);
+            SpawnSlime();
         }
-        
-        
+
         SlimeCountClear();
+
+        if (currentStack < maxStack)
+        {
+            stackTimer += Time.deltaTime;
+            if (stackTimer >= coolDown)
+            {
+                currentStack++;
+                stackTimer = 0;
+            }
+        }
     }
     //슬라임을 생성하는 메서드
     public void SpawnSlime()  
     {
+        if (currentStack <= 0)
+            return;
+
         currentSlime = Instantiate(slimePrefab, spawnPosition.position,Quaternion.identity);
 
         Slime slime = currentSlime.GetComponent<Slime>();
         slime.towerRoot = towerRoot;
-        
+
+        currentStack--;
     }
     //떨어지는 메서드(움직이는 물체를 보여줘야하니까)
     public void MoveSlime()
