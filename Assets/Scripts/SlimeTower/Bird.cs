501744cba14eb4d73baea735b93dfbc7c9beb054
Hae1385 <206494390+Hae1385@users.noreply.github.com>
2025-07-07 20:20:18 +0900

    Add : 슬라임타워
    쿨타임 기능 추가

--- Assets/Scripts/SlimeTower/Bird.cs
+++ Assets/Scripts/SlimeTower/Bird.cs
@@ -1,18 +1,21 @@
-﻿using System;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
     //새가 좌우로 움직이는 역할을 담당할꺼야
-   public float birdSpeed = 2f;
-   private bool isMove = true;
-   private Vector3 direction;
-   public SpriteRenderer BirdSprite;
+    public float birdSpeed = 2f;
+    private bool isMove = true;
+    private Vector3 direction;
+    public SpriteRenderer BirdSprite;
+    
+    public BoxCollider2D[] boxColliders;
+    public Vector2 birdTransform;
+    private const float MARGIN = 2f;
 
 
-   void Start()
-   {
+    void Start()
+    {
         if (StageManager.instance != null && GameManager.Instance)
         {
             int difficulty = StageManager.instance.difficulty;
@@ -25,30 +28,46 @@ public class Bird : MonoBehaviour
             }
         }
 
-       direction = Vector3.right;
-   }
-   void Update()
-   {
-       BirdMove();
-   }
+        float minX = float.MaxValue;
+        float maxX = float.MinValue;
 
-//새가 좌우로 움직이게 만들려면 일단 초기 방향을 잡아주고
-//어떤 속도로 이동할껀지 생각을 해보자.
-   public void BirdMove()
-   {
-       if (isMove)
-       {
-           transform.Translate(-direction * birdSpeed * Time.deltaTime);
-       }
-   }
+        foreach (BoxCollider2D collider in boxColliders)
+        {
+            if(collider.bounds.min.x < minX)
+                minX = collider.bounds.min.x + MARGIN;
+            if(collider.bounds.max.x > maxX)
+                maxX = collider.bounds.max.x - MARGIN;
+        }
+        
+        Vector3 pos = transform.position;
+        pos.x = Random.Range(minX, maxX);
+        transform.position = pos;
 
-   
-   public void OnTriggerEnter2D(Collider2D other)
-   {
-       if (other.CompareTag("TurnZone"))
-       {
-           direction = -direction;
-           BirdSprite.flipX = direction.x < 0;
-       }
-   }
+        direction = Random.value < 0.5f ? Vector3.right : Vector3.left;
+        BirdSprite.flipX = direction.x < 0;
+    }
+    void Update()
+    {
+        BirdMove();
+    }
+
+    //새가 좌우로 움직이게 만들려면 일단 초기 방향을 잡아주고
+    //어떤 속도로 이동할껀지 생각을 해보자.
+    public void BirdMove()
+    {
+        if (isMove)
+        {
+            transform.Translate(-direction * birdSpeed * Time.deltaTime);
+        }
+    }
+
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("TurnZone"))
+        {
+            direction = -direction;
+            BirdSprite.flipX = direction.x < 0;
+        }
+    }
 }
