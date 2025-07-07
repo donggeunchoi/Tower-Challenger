055d742cdfae5ec1399c9810a27b2a7b705f593b
whomery <whomery12345@gmail.com>
2025-07-07 21:00:30 +0900

    fix : 파일이동

--- Assets/Scripts/MiniGames/HardGame/HardGameGameManager.cs
+++ Assets/Scripts/MiniGames/HardGame/HardGameGameManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Unity.VisualScripting;
+using UnityEngine;
 
 public class HardGameGameManager : MonoBehaviour
 {
@@ -31,6 +32,30 @@ public class HardGameGameManager : MonoBehaviour
             Vector3 rot = transform.eulerAngles;
             rot.y += 180f;
             transform.rotation = Quaternion.Euler(rot);
+        }
+    }
+
+    public void NextLv()
+    {
+        for (int i = 0; i < Lv.Length - 1; i++)
+        {
+            if (Lv[i].activeSelf)
+            {
+                Lv[i].SetActive(false);//기존 스테이지 지우고
+                Lv[i + 1].SetActive(true);
+
+                if (Player)
+                {
+                    switch (i + 1)
+                    {
+                        case 1: //2스테이지
+                            Player.transform.position = new Vector3(-8f, 4.5f, Player.transform.position.z);//시작위치
+                            moveSpeed = moveSpeed;//속도
+                            break;
+                    }
+                }
+                return;
+            }
         }
     }
 }
