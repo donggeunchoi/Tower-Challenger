095a0680497186d7f72a2a0846c316a9350acea6
ChaWooJin <chawoojin06@gmail.com>
2025-07-08 22:08:57 +0900

    update : player joy stick move

---
+++ Assets/Scripts/CWJ_TitanSoul/MapLayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MapLayer : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        int layerOne = LayerMask.NameToLayer("Layer 1");
+        int layerTow = LayerMask.NameToLayer("Layer ");
+
+        if (collision.CompareTag("Player"))
+        {
+            if (this.gameObject.layer == layerOne)
+            {
+                collision.gameObject.layer = layerOne;
+            }
+            else if (this.gameObject.layer == layerTow)
+            {
+                collision.gameObject.layer = layerTow;
+            }
+        }
+    }
+}
