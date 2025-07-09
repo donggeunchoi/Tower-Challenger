c9a6e3da774cf4011abcd8cd0956f86a2402296e
ChaWooJin <chawoojin06@gmail.com>
2025-07-09 21:23:37 +0900

    update : boss enemy move

--- Assets/Scripts/CWJ_TitanSoul/MapLayer.cs
+++ Assets/Scripts/CWJ_TitanSoul/MapLayer.cs
@@ -5,9 +5,20 @@ public class MapLayer : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         int layerOne = LayerMask.NameToLayer("Layer 1");
-        int layerTow = LayerMask.NameToLayer("Layer ");
+        int layerTow = LayerMask.NameToLayer("Layer 2");
 
         if (collision.CompareTag("Player"))
+        {
+            if (this.gameObject.layer == layerOne)
+            {
+                collision.gameObject.layer = layerOne;
+            }
+            else if (this.gameObject.layer == layerTow)
+            {
+                collision.gameObject.layer = layerTow;
+            }
+        }
+        else if (collision.CompareTag("Boss"))
         {
             if (this.gameObject.layer == layerOne)
             {
