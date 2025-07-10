d745efd66415384327c0754bbd7f37d7022bd514
Hae1385 <206494390+Hae1385@users.noreply.github.com>
2025-07-11 01:32:35 +0900

    Fix : 불필요한 파일 정리

---
+++ Assets/03 Scripts/Village/VillageManager.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class VillageManager : MonoBehaviour
+{
+    [Header("fade")]
+    public Image fadePanel;
+    public float fadeDuration;
+    
+    [Header("아이템 상점")]
+    public GameObject StorePanel;
+    
+    [Header("인벤토리")]
+    public GameObject Invetory;
+
+    [Header("우편함")] 
+    public GameObject MailBox;
+
+    [Header("옷가게")] 
+    public GameObject ClothesShopPanel;
+    public GameObject ClothesShopItem;
+
+    [Header("길드")] 
+    public GameObject Guild;
+
+    [Header("설정")] 
+    public GameObject StopPanel;
+    
+    [Header("캐릭터 선택")]
+    public GameObject CharactorChoicePanel;
+
+    [Header("판넬 관리")]
+    public Transform popupGroup;
+    public GameObject popup;
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        fadePanel.gameObject.SetActive(true);
+        fadePanel.color = new Color(0, 0, 0, 1);
+        StartCoroutine(FadeOut());
+    }
+
+    IEnumerator FadeOut()
+    {
+        float time = 0;
+        while (time < fadeDuration)
+        {
+            time += Time.deltaTime;
+            float alpha = Mathf.Lerp(1, 0, time/fadeDuration);
+            fadePanel.color = new Color(0, 0, 0, alpha);
+            yield return null;
+        }
+
+        fadePanel.gameObject.SetActive(false);
+    }
+
+    public void VillageMove(string villageName)
+    {
+        switch (villageName)
+        {
+            case "StorePanel":
+                if (popup != null)
+                    Destroy(popup);
+                popup = Instantiate(ClothesShopPanel, popupGroup);
+                break;
+            case "Invetory":
+                if (popup != null)
+                    Destroy(popup);
+                popup = Instantiate(Invetory, popupGroup);
+                break;
+            case "MailBox":
+                MailBox.SetActive(true);
+                break;
+            case "ClothesShopPanel":
+                if (popup != null)
+                    Destroy(popup);
+                popup = Instantiate(StorePanel, popupGroup);
+                break;
+            case "Guild":
+                if (popup != null)
+                    Destroy(popup);
+                popup = Instantiate(Guild, popupGroup);
+                break;
+            case "PausePanel":
+                StopPanel.SetActive(true);
+                break;
+            case "CharactorChoicePanel":
+                if (popup != null)
+                    Destroy(popup);
+                popup = Instantiate(CharactorChoicePanel, popupGroup);
+                break;
+            case "TowerEntrance":
+                SceneManager.LoadScene("TowerEntrance");
+                break;
+        }
+    }
+    
+
+    #region 옷가게
+
+    public void OnClickClothesShopClose()
+    {
+        ClothesShopPanel.SetActive(false);
+    }
+
+    public void OnClickClothesItemOpen()
+    {
+        ClothesShopItem.SetActive(true);
+    }
+
+    public void OnClickClothesShopItemClose()
+    {
+        ClothesShopItem.SetActive(false);
+    }
+
+    #endregion
+
+    public void OnClickMailBox()
+    {
+        MailBox.SetActive(true);
+    }
+
+    public void OnClickMailBoxClose()
+    {
+        MailBox.SetActive(false);
+    }
+    
+    
+}
