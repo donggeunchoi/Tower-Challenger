364b28efe6d8326a1529092106aa72683ec0de65
donggeunchoi <cdg01063149045@gmail.com>
2025-07-10 16:25:16 +0900

    add : 스피드 퀴즈 데이터 테이블 업로드

---
+++ Assets/Scripts/MiniGames/SpeedQuizGame/SpeedQuizData.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[System.Serializable]
+public class SpeedQuizData : MonoBehaviour
+{
+    public string index;
+    public string name;
+    public string question;
+    public string answer;
+    public string op2;
+    public string op3;
+    public string op4;
+}
