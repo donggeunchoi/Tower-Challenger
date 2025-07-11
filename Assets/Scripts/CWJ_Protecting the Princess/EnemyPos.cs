aee6917a6443fe909196c7c6ace7be89bdee95bd
ChaWooJin <chawoojin06@gmail.com>
2025-07-11 10:06:19 +0900

    Add : 파이어볼 이미지 prefab으로 변경

---
+++ Assets/Scripts/CWJ_Protecting the Princess/EnemyPos.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPos : MonoBehaviour
+{
+    [SerializeField] private GameObject[] enemyPrefabs;
+
+    private List<GameObject>[] pools;
+
+    private void Start()
+    {
+        pools = new List<GameObject>[enemyPrefabs.Length];
+
+        float x = Random.Range(-11f, 10);
+        float y = Random.Range(-1f, 5.5f);
+
+        transform.position = new Vector2(x, y);
+    }
+}
