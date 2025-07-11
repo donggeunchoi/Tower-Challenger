d0077853898cdc9bba3d83a25d72455118f7296a
ChaWooJin <chawoojin06@gmail.com>
2025-07-11 18:02:28 +0900

    fix : 그림자 게임 스크립터블오브젝트

--- Assets/03 Scripts/CWJ_Protecting the Princess/EnemyPos.cs
+++ Assets/03 Scripts/CWJ_Protecting the Princess/EnemyPos.cs
@@ -3,17 +3,62 @@ using UnityEngine;
 
 public class EnemyPos : MonoBehaviour
 {
+    // 프리펩을 넣는 변수
     [SerializeField] private GameObject[] enemyPrefabs;
 
+    // 프리펩이 들어있는 
     private List<GameObject>[] pools;
+
+    private int randomEnemy;
+    
 
     private void Start()
     {
-        pools = new List<GameObject>[enemyPrefabs.Length];
+        PrefabInit();
+    }
 
-        float x = Random.Range(-11f, 10);
-        float y = Random.Range(-1f, 5.5f);
+    private void Update()
+    {
+       
 
-        transform.position = new Vector2(x, y);
+        if (Input.GetButtonDown("Jump"))
+        {
+            randomEnemy = Random.Range(0, enemyPrefabs.Length);
+            GetEnemy(randomEnemy);
+        }
+    }
+
+    private void PrefabInit()
+    {
+        pools = new List<GameObject>[enemyPrefabs.Length + 1];
+
+        for (int i = 0; i < pools.Length; i++)
+        {
+            pools[i] = new List<GameObject>();
+        }
+    }
+
+    private GameObject GetEnemy(int index)
+    {
+        GameObject select = null;
+        foreach (GameObject enemy in pools[index])
+        {
+            if (!enemy.activeSelf)
+            {
+                select = enemy;
+                select.SetActive(true);
+
+                break;
+            }
+        }
+
+        if (select == null)
+        {
+            // 원본 오브젝트를 복사해서 생성하는 함수
+            select = Instantiate(enemyPrefabs[index], transform);
+            pools[index].Add(select);
+        }
+
+        return select;
     }
 }
--- Assets/03 Scripts/CWJ_Shadow/ShadowManager.cs
+++ Assets/03 Scripts/CWJ_Shadow/ShadowManager.cs
@@ -2,14 +2,8 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-[CreateAssetMenu(fileName = "ShadowData", menuName = "UI/Data")]
-public class ShadowData : ScriptableObject
-{
-    public Sprite shadowSprite; // 그림자 이미지
-    public Sprite[] choices; // 선택지 이미지
-    public float shadowTime; // 그림자 이미지가 보여지는 시간
-    public int successIndex;
-}
+
+
 public class ShadowManager : MonoBehaviour
 {
     public static ShadowManager instance;
@@ -17,10 +11,6 @@ public class ShadowManager : MonoBehaviour
     public Shadow shadow;
     public ShadowUI shadowUI;
     public ShadowData[] shadowData;
-
-    public float time;
-
-
     private void Awake()
     {
         if (instance == null)
@@ -32,12 +22,6 @@ public class ShadowManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        for (int i = 0; i < shadowData.Length; i++)
-        {
-            shadowData[i].shadowTime = time;
-        }
-        
         shadowUI.shadowGameInit();
     }
-
 }
