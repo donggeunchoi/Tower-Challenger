b821c15449dfc972067fd044da0c821629acc343
ChaWooJin <chawoojin06@gmail.com>
2025-07-11 10:46:14 +0900

    fix : 그림자 게임 데이터 테이블 수정

--- Assets/03 Scripts/CWJ_Shadow/ShadowManager.cs
+++ Assets/03 Scripts/CWJ_Shadow/ShadowManager.cs
@@ -2,6 +2,14 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+[CreateAssetMenu(fileName = "ShadowData", menuName = "UI/Data")]
+public class ShadowData : ScriptableObject
+{
+    public Sprite shadowSprite; // 그림자 이미지
+    public Sprite[] choices; // 선택지 이미지
+    public float shadowTime; // 그림자 이미지가 보여지는 시간
+    public int successIndex;
+}
 public class ShadowManager : MonoBehaviour
 {
     public static ShadowManager instance;
