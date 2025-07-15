3fc1982ef9c347c91f8aebf1d627793dd42b4795
Hae1385 <206494390+Hae1385@users.noreply.github.com>
2025-07-15 12:12:54 +0900

    Fix : 스테미나 UI 타이머 수정

--- Assets/03 Scripts/Village/StaminaCharge.cs
+++ Assets/03 Scripts/Village/StaminaCharge.cs
@@ -1,19 +1,15 @@
-using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class StaminaCharge : MonoBehaviour
 {
     public GameObject timeText;
-    GameManager gameManager;
-
-    private void Awake()
-    {
-        gameManager = FindObjectOfType<GameManager>();
-    }
 
     void Update()
     {
-        string Show = $"{(gameManager.staminatimer * 1/60f).ToString("N0")} / 30";
-        timeText.GetComponent<TMPro.TMP_Text>().text = Show;
+        if (GameManager.Instance != null)
+        {
+            string Show = $"{(GameManager.Instance.staminatimer / 60f).ToString("N0")} / 30";
+            timeText.GetComponent<TMPro.TMP_Text>().text = Show;
+        }
     }
 }
