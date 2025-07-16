483283524d46d9e6932fa9658474b6bcf1516811
donggeunchoi <cdg01063149045@gmail.com>
2025-07-16 10:43:55 +0900

    Add : 튜토리얼 패널 생성

--- Assets/03 Scripts/Tutorial/TutorialManager.cs
+++ Assets/03 Scripts/Tutorial/TutorialManager.cs
@@ -1,20 +1,68 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TutorialManager : MonoBehaviour
 {
-    
-    
     public Image fadeImage;
     public float fadeDuration;
+    public List<GameObject> tutorialTextUI;
+
+    private int currentTutorial = 0;
+    private bool isWaitingForInput = false;
 
     private void Start()
     {
-        StartCoroutine(FadeOut());
+        foreach (GameObject obj in tutorialTextUI)
+        {
+            obj.SetActive(false);
+        }
+        
+        StartCoroutine(StartTutorial());
+    }
+
+    IEnumerator StartTutorial()
+    {
+        yield return StartCoroutine(FadeOut());
+        tutorialTextUI[0].SetActive(true);
+        isWaitingForInput = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isWaitingForInput && Input.GetKeyDown(KeyCode.Space))
+        {
+            isWaitingForInput = true;
+            currentTutorial++;
+
+            if (currentTutorial < tutorialTextUI.Count)
+            {
+                ShowOnlyPanel(currentTutorial);
+                isWaitingForInput = true;
+            }
+            else
+            {
+                Debug.Log("이제 포탈을 이용해야할때");
+            }
+        }
+    }
+
+    private void ShowOnlyPanel(int index)
+    {
+        for (int i = 0; i < tutorialTextUI.Count; i++)
+        {
+            tutorialTextUI[i].SetActive(i == index);
+        }
+    }
+
+    IEnumerator EndTutoreal()
+    {
+        yield return StartCoroutine(FadeIn());
     }
     
-
+    
     IEnumerator FadeIn()//투명에서 검은거로 
     {
         float time = 0f;
@@ -41,11 +89,5 @@ public class TutorialManager : MonoBehaviour
             yield return null;
         }
         fadeImage.color = new Color(0, 0, 0, 0);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
