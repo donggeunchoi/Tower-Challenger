24a3b2e9563a101cdef7140c0bf249550f82bde8
donggeunchoi <cdg01063149045@gmail.com>
2025-07-20 15:03:08 +0900

    fix : ScoreCount -> SlimeJumpManager 이동 2

--- Assets/03 Scripts/MiniGames/CatTowerJump/SlimeJumpManager.cs
+++ Assets/03 Scripts/MiniGames/CatTowerJump/SlimeJumpManager.cs
@@ -1,4 +1,5 @@
 using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,30 +7,56 @@ public class SlimeJumpManager : MonoBehaviour
 {
     public static SlimeJumpManager Instance;
     
-    public BackGroundSpawner BackGroundspawner;
-    public WallSpawner wallSpawner;
-    public ObstacleSpawner obstacleSpawner;
+    [Header("References")]
+    public Transform playerTransform;
+    public TextMeshProUGUI TargetText;
+    public TextMeshProUGUI CurrnetText;
 
+    private float startY;
+    [SerializeField]private float TargetY;
+    
+    
     private void Awake()
     {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(this);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
+       Instance = this;
     }
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startY =playerTransform.position.y;
+        UpdateText(100);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float currentY = playerTransform.position.y;
+        if (currentY > TargetY)
+        {
+            //여기가 게임 클리어
+            // SlimeJumpManager.Instance.ClearGame();
+        }
+        
+        CurrnetText.text = $"{currentY.ToString("N0")}m";
+        
+    }
+    private void UpdateText(float value)
+    {
+        int currentY = Mathf.FloorToInt(value);
+        TargetText.text = $"{currentY.ToString()}m";
+    }
+
 
     public void ClearGame()
     {
+        StageManager.instance.MiniGameResult(true);
         //이쪽에 다시 씬으로 넘기기
     }
 
-    public void GameOver()
+    public void SlimeHit()
     {
+        StageManager.instance.MiniGameResult(false);
         //여기에 패널 열어뿌지뭐
     }
 }
