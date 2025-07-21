5ced2d319d5abf4fad912aca185e71d173ef3cfb
ChaWooJin <chawoojin06@gmail.com>
2025-07-21 10:58:18 +0900

    fix : 공주지키기 매니저 복구

---
+++ Assets/03 Scripts/CWJ_Princess/PrincessManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PrincessManager : MonoBehaviour
+{
+    public static PrincessManager princessInstance;
+
+    public ShieldMove shieldMove;
+    public EnemyPos enemyPos;
+    public SpriteRenderer spriteRenderer;
+
+
+    [Header("박쥐 이동속도")]
+    public float batSpeed;
+    [Header("박쥐가 최대로 올라가는 y값")]
+    public float maxY;
+    [Header("박쥐다 최대로 내려가는 y값")]
+    public float minY;
+    [Header("박쥐의 y값 이동의 속도")]
+    public float frequency;
+    [Header("몬스터가 생성되는 주기")]
+    public float enemyTime;
+
+    [Header("불덩이 이동속도")]
+    public float fireSpeed;
+    private void Awake()
+    {
+        if (princessInstance == null)
+        {
+            princessInstance = this;
+        }
+
+    }
+
+    private void Start()
+    {
+        spriteRenderer.gameObject.SetActive(false);
+    }
+}
--- Assets/03 Scripts/CWJ_Protecting the Princess/ObjDestroy.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class ObjDestroy : MonoBehaviour
-{
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Destroy(collision.gameObject);
-    }
-}
