056cfe4e9d3ea80245fee00b92df7385d1983934
whomery <whomery12345@gmail.com>
2025-07-21 17:54:11 +0900

    Update : 알게임 알디자인 수정

---
+++ Assets/03 Scripts/MiniGames/EggGame/EggClickHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class EggClickHandler : MonoBehaviour
+{
+    public EggGameManager gameManager;
+
+    private void OnMouseDown()
+    {
+        if (gameManager != null)
+        {
+            gameManager.EggClick();
+        }
+        else
+        {
+            Debug.LogWarning("EggClickHandler: gameManager가 연결되지 않았습니다.");
+        }
+    }
+}
