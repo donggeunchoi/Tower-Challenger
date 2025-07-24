e1d5856e1810842f71f825dda3ffcf3bf7b3a668
Hae1385 <206494390+Hae1385@users.noreply.github.com>
2025-07-24 15:56:19 +0900

    Add : 개발자 전용 키보드 인풋 추가

--- Assets/03 Scripts/Player/PlayerDevInput.cs
+++ Assets/03 Scripts/Player/PlayerDevInput.cs
@@ -1,16 +1,52 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerDevInput : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Vector2 keyboardInput = Vector2.zero;
+
+    public PlayerInput playerInput;
+    public PlayerInteraction interaction;
+
+    private void FixedUpdate()
     {
-        
+        if (playerInput == null) return;
+
+        if (keyboardInput.sqrMagnitude > 0.01f)
+        {
+            playerInput.SetKeyboardInput(keyboardInput);
+        }
+        else
+        {
+            playerInput.SetKeyboardInput(Vector2.zero);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnMove(InputAction.CallbackContext context)
     {
-        
+        if (context.phase == InputActionPhase.Performed || context.phase == InputActionPhase.Started)
+        {
+            keyboardInput = context.ReadValue<Vector2>();
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            keyboardInput = Vector2.zero;
+        }
+    }
+
+    public void OnDash(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed && playerInput != null)
+        {
+            playerInput.OnClickDash();
+        }
+    }
+
+    public void OnInteraction(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed && interaction != null)
+        {
+            interaction.Interact();
+        }
     }
 }
