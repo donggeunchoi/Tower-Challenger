4b7db54604f7cfa66040ae58ee75511d674b27d4
whomery <whomery12345@gmail.com>
2025-07-29 19:46:31 +0900

    update : 어려운 게임 치트키 추가

--- Assets/03 Scripts/GameManager.cs
+++ Assets/03 Scripts/GameManager.cs
@@ -75,6 +75,7 @@ public class GameManager : MonoBehaviour
         {
             if (storyManagerPrefab != null)
                 Instantiate(storyManagerPrefab);
+        }
 
         if (FindAnyObjectByType(typeof(PoolManager)) == null)
         {
