cbbb71f737de73be718ef9af8a2d7bfb6861e665
donggeunchoi <cdg01063149045@gmail.com>
2025-07-31 11:02:40 +0900

    Update : 빌리지 튜토리얼 제작 업데이트

---
+++ Assets/03 Scripts/Village/Tutorial/TutorialVillage.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialVillage : MonoBehaviour
+{
+    public List<GameObject> Tutorial = new List<GameObject>();
+
+    public void OnClickNext()
+    {
+        
+        ShowUI();
+    }
+
+    public void OnClickBack()
+    {
+        
+        ShowUI();
+        
+        
+    }
+
+    public void ShowUI()
+    {
+        
+    }
+    
+}
