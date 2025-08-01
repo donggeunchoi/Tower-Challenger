fc67ab5614457fb1c54adf80b92546650e5d9d3c
Hae1385 <206494390+Hae1385@users.noreply.github.com>
2025-08-01 21:24:24 +0900

    Fix : 시작씬 영상 코루틴 수정

--- Assets/03 Scripts/StartManager.cs
+++ Assets/03 Scripts/StartManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -11,22 +12,16 @@ public class StartManager : MonoBehaviour
     public Button startButton;
     public GameObject startPanel;
     public Image fadeImage;
-    public float fadeDuration = 1.0f;
-
-    private bool videoFinished = false;
-    private bool videoStarted = false;
+    public float fadeDuration;
 
     void Start()
     {
         videoPanel.SetActive(false);
         fadeImage.gameObject.SetActive(false);
         fadeImage.color = new Color(0, 0, 0, 0);
-
+        
         startButton.onClick.AddListener(OnStartButtonClick);
-
-        videoPlayer.loopPointReached += OnVideoEnd;
-        videoFinished = false;
-        videoStarted = false;
+        
     }
 
     private void OnStartButtonClick()
@@ -34,87 +29,67 @@ public class StartManager : MonoBehaviour
         StartCoroutine(PlayVideoSequence());
     }
 
-    void OnVideoEnd(VideoPlayer vp)
-    {
-        if (videoFinished) return;
-        videoFinished = true;
-        StartCoroutine(EndSequenceAndLoadScene());
-    }
-
-    void Update()
-    {
-        // 혹시 이벤트가 안 들어왔을 때, 마지막 프레임 감시 - "영상이 끝과 동시에 씬 전환"
-        if (!videoFinished && videoStarted && videoPlayer.isPlaying && videoPlayer.frame > 0 &&
-            videoPlayer.frame >= (long)videoPlayer.frameCount - 1)
-        {
-            OnVideoEnd(videoPlayer);
-        }
-    }
-
     IEnumerator PlayVideoSequence()
     {
         fadeImage.gameObject.SetActive(true);
         yield return StartCoroutine(FadeIn());
-
+        
         videoPanel.SetActive(true);
         startPanel.SetActive(false);
 
-        videoFinished = false;
-        videoStarted = true;
         videoPlayer.Play();
-
+        
         yield return StartCoroutine(FadeOut());
 
-        // 영상 끝날 때까지 대기(타임아웃 병행, 영상 길이+2초)
-        float maxWait = (float)videoPlayer.length + 2.0f;
-        float timer = 0f;
-        while (!videoFinished && timer < maxWait)
+        yield return StartCoroutine(WaitforVideoToEnd());
+    }
+
+    IEnumerator WaitforVideoToEnd()
+    {
+        videoPlayer.Prepare();
+        while (!videoPlayer.isPrepared)
+            yield return null;
+
+        float vidioPlaying = (float)videoPlayer.length - 2.65f;
+        float timer = 0;
+
+        Debug.Log(vidioPlaying);
+
+        while (timer < vidioPlaying)
         {
             timer += Time.deltaTime;
             yield return null;
         }
-        // 만약 끝까지 안 들어오면 강제 종료
-        if (!videoFinished)
-        {
-            OnVideoEnd(videoPlayer);
-        }
-    }
 
-    IEnumerator EndSequenceAndLoadScene()
-    {
         videoPanel.SetActive(false);
-
-        fadeImage.gameObject.SetActive(true);
-        yield return StartCoroutine(FadeIn());
-
-        yield return new WaitForSeconds(0.2f);
         SceneManager.LoadScene("VillageScene");
     }
 
-    IEnumerator FadeIn()
+    IEnumerator FadeIn()//투명에서 검은거로 
     {
         float time = 0f;
         while (time < fadeDuration)
         {
             time += Time.deltaTime;
-            float alpha = Mathf.Lerp(0, 1, time / fadeDuration);
+            float alpha = Mathf.Lerp(0,1, time/fadeDuration);
             fadeImage.color = new Color(0, 0, 0, alpha);
             yield return null;
         }
+        
         fadeImage.color = new Color(0, 0, 0, 1);
     }
 
-    IEnumerator FadeOut()
+    IEnumerator FadeOut()//검은거에서 투명으로
     {
         float time = 0f;
+
         while (time < fadeDuration)
         {
             time += Time.deltaTime;
-            float alpha = Mathf.Lerp(1, 0, time / fadeDuration);
+            float alpha = Mathf.Lerp(1, 0, time/fadeDuration);
             fadeImage.color = new Color(0, 0, 0, alpha);
             yield return null;
         }
         fadeImage.color = new Color(0, 0, 0, 0);
-        fadeImage.gameObject.SetActive(false);
     }
 }
