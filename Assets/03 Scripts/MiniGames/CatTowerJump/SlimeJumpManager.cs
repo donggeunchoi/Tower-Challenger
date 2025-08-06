266d1bea88921733ac02fe50b06fbdce8c277daf
Hae1385 <206494390+Hae1385@users.noreply.github.com>
2025-08-06 12:51:24 +0900

    Add : 게임씬 플레이어 스포너 추가

--- Assets/03 Scripts/MiniGames/CatTowerJump/SlimeJumpManager.cs
+++ Assets/03 Scripts/MiniGames/CatTowerJump/SlimeJumpManager.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections;
 using TMPro;
-using Unity.Android.Gradle.Manifest;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class SlimeJumpManager : MonoBehaviour
 {
