fc7ffca07766be4c52c9322bab36adc17db61abe
Hae1385 <206494390+Hae1385@users.noreply.github.com>
2025-08-07 22:14:07 +0900

    Add : 구조물 난이도별 할당

---
+++ Assets/03 Scripts/Stage/DiffcultyObstacles.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Linq;
+
+public class DiffcultyObstacles : MonoBehaviour
+{
+    [SerializeField] private Trap[] trabs;
+    [SerializeField] private InteractionBox[] interactionBoxs;
+    public void Init()
+    {
+        SetTrab();
+        SetBox();
+    }
+
+    private void SetTrab()
+    {
+        if (StageManager.instance != null && StageTable.trapCountTableList.Find(x => x.floor == StageManager.instance.floor) != null)
+        {
+            Debug.Log("트랩활성화");
+
+            int oneTrapPer = StageTable.trapCountTableList.Find(x => x.floor == StageManager.instance.floor).oneDisPer;
+            int twoTrapPer = StageTable.trapCountTableList.Find(x => x.floor == StageManager.instance.floor).twoDisPer;
+            int thrTrapPer = StageTable.trapCountTableList.Find(x => x.floor == StageManager.instance.floor).thrDisPer;
+            int fourTrapPer = StageTable.trapCountTableList.Find(x => x.floor == StageManager.instance.floor).foDisPer;
+            int fivTrapPer = StageTable.trapCountTableList.Find(x => x.floor == StageManager.instance.floor).fiveDisPer;
+
+            int randomTrapMax = oneTrapPer + twoTrapPer + thrTrapPer + fourTrapPer + fivTrapPer;
+            int randomTrap = Random.Range(1, randomTrapMax + 1);
+
+            int trapCount;
+            if (randomTrap < oneTrapPer)
+                trapCount = 1;
+            else if (randomTrap < oneTrapPer + twoTrapPer)
+            {
+                trapCount = 2;
+            }
+            else if (randomTrap < oneTrapPer + twoTrapPer + thrTrapPer)
+            {
+                trapCount = 3;
+            }
+            else if (randomTrap < oneTrapPer + twoTrapPer + thrTrapPer + fourTrapPer)
+            {
+                trapCount = 4;
+            }
+            else
+            {
+                trapCount = 5;
+            }
+            int activeTrabs = Random.Range(0, Mathf.Min(trapCount, trabs.Length));
+
+            Trap[] shuffleTrabs = trabs.OrderBy(x => Random.value).ToArray();
+
+            for (int i = 0; i < shuffleTrabs.Length; i++)
+            {
+                if (i <= activeTrabs)
+                {
+                    shuffleTrabs[i].gameObject.SetActive(true);
+                }
+                else
+                {
+                    shuffleTrabs[i].DestroyTrapInfo();
+                    shuffleTrabs[i].gameObject.SetActive(false);
+                }
+            }
+
+            Debug.Log("트랩" + trapCount + "개");
+        }
+    }
+    private void SetBox()
+    {
+        if (StageManager.instance != null && BoxDataTable.boxDataList.Find(x => x.floor == StageManager.instance.floor) != null)
+        {
+            int oneBoxPer = BoxDataTable.boxDataList.Find(x => x.floor == StageManager.instance.floor).oneBoxPer;
+            int twoBoxPer = BoxDataTable.boxDataList.Find(x => x.floor == StageManager.instance.floor).thrBoxPer;
+            int thrBoxPer = BoxDataTable.boxDataList.Find(x => x.floor == StageManager.instance.floor).thrBoxPer;
+
+            int randomBoxMax = oneBoxPer + twoBoxPer + thrBoxPer;
+            int randomBox = Random.Range(1, randomBoxMax + 1);
+
+            int BoxCount;
+
+            if (randomBox < oneBoxPer)
+                BoxCount = 1;
+            else if (randomBox < oneBoxPer + twoBoxPer)
+            {
+                BoxCount = 2;
+            }
+            else
+            {
+                BoxCount = 3;
+            }
+
+            InteractionBox[] shuffleBoxs = interactionBoxs.OrderBy(x => Random.value).ToArray();
+
+            int activeBox = Random.Range(0, Mathf.Min(BoxCount, interactionBoxs.Length));
+
+            for (int i = 0; i < shuffleBoxs.Length; i++)
+            {
+                if (i <= activeBox)
+                {
+                    shuffleBoxs[i].gameObject.SetActive(true);
+                }
+                else
+                {
+                    shuffleBoxs[i].DestroyBoxInfo();
+                    shuffleBoxs[i].gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
--- Assets/03 Scripts/Stage/MapObstacle.cs
+++ Assets/03 Scripts/Stage/MapObstacle.cs
@@ -1,112 +1,36 @@
-﻿using Unity.VisualScripting;
-using UnityEngine;
-using System.Linq;
+﻿using UnityEngine;
 
 public class MapObstacle : MonoBehaviour
 {
-
-    [SerializeField] private Trap[] trabs;
-    [SerializeField] private InteractionBox[] interactionBoxs;
+    [SerializeField] private DiffcultyObstacles[] obstacles;
 
     [SerializeField] private int difficulty;
 
     public void Init()
     {
-        SetTrab();
-        SetBox();
-    }
+        this.gameObject.SetActive(true);
 
-    private void SetTrab()
-    {
-        if (StageManager.instance != null && StageTable.trapCountTableList.Find(x => x.floor == StageManager.instance.floor) != null)
+        for (int i = 0; i < obstacles.Length; i++)
         {
-            int oneTrapPer = StageTable.trapCountTableList.Find(x => x.floor == StageManager.instance.floor).oneDisPer;
-            int twoTrapPer = StageTable.trapCountTableList.Find(x => x.floor == StageManager.instance.floor).twoDisPer;
-            int thrTrapPer = StageTable.trapCountTableList.Find(x => x.floor == StageManager.instance.floor).thrDisPer;
-            int fourTrapPer = StageTable.trapCountTableList.Find(x => x.floor == StageManager.instance.floor).foDisPer;
-            int fivTrapPer = StageTable.trapCountTableList.Find(x => x.floor == StageManager.instance.floor).fiveDisPer;
+            obstacles[i].gameObject.SetActive(false);
+        }
 
-            int randomTrapMax = oneTrapPer + twoTrapPer + thrTrapPer + fourTrapPer + fivTrapPer;
-            int randomTrap = Random.Range(1, randomTrapMax + 1);
+        int diffcultyIndex = CheckDiffculty();
 
-            int trapCount;
-            if (randomTrap < oneTrapPer)
-                trapCount = 1;
-            else if (randomTrap < oneTrapPer + twoTrapPer)
-            {
-                trapCount = 2;
-            }
-            else if (randomTrap < oneTrapPer + twoTrapPer + thrTrapPer)
-            {
-                trapCount = 3;
-            }
-            else if (randomTrap < oneTrapPer + twoTrapPer + thrTrapPer + fourTrapPer)
-            {
-                trapCount = 4;
-            }
-            else
-            {
-                trapCount = 5;
-            }
-            int activeTrabs = Random.Range(0, Mathf.Min(trapCount, trabs.Length));
-
-            Trap[] shuffleTrabs = trabs.OrderBy(x => Random.value).ToArray();
-
-            for (int i = 0; i < shuffleTrabs.Length; i++)
-            {
-                if (i < activeTrabs)
-                {
-                    shuffleTrabs[i].gameObject.SetActive(true);
-                }
-                else
-                {
-                    shuffleTrabs[i].DestroyTrapInfo();
-                    shuffleTrabs[i].gameObject.SetActive(false);
-                }
-            }
+        if (diffcultyIndex >= 0 && diffcultyIndex < obstacles.Length && obstacles[diffcultyIndex] != null)
+        {
+            obstacles[diffcultyIndex].gameObject.SetActive(true);
+            obstacles[diffcultyIndex].Init();
         }
     }
 
-    private void SetBox()
+    public int CheckDiffculty()
     {
-        if (StageManager.instance != null && BoxDataTable.boxDataList.Find(x => x.floor == StageManager.instance.floor) != null)
+        if (StageManager.instance != null)
         {
-            int oneBoxPer = BoxDataTable.boxDataList.Find(x => x.floor == StageManager.instance.floor).oneBoxPer;
-            int twoBoxPer = BoxDataTable.boxDataList.Find(x => x.floor == StageManager.instance.floor).thrBoxPer;
-            int thrBoxPer = BoxDataTable.boxDataList.Find(x => x.floor == StageManager.instance.floor).thrBoxPer;
-
-            int randomBoxMax = oneBoxPer + twoBoxPer + thrBoxPer;
-            int randomBox = Random.Range(1, randomBoxMax + 1);
-
-            int BoxCount;
-
-            if (randomBox < oneBoxPer)
-                BoxCount = 1;
-            else if (randomBox < oneBoxPer + twoBoxPer)
-            {
-                BoxCount = 2;
-            }
-            else
-            {
-                BoxCount = 3;
-            }
-
-            InteractionBox[] shuffleBoxs = interactionBoxs.OrderBy(x => Random.value).ToArray();
-
-            int activeBox = Random.Range(0, Mathf.Min(BoxCount, interactionBoxs.Length));
-
-            for (int i = 0; i < shuffleBoxs.Length; i++)
-            {
-                if (i < activeBox)
-                {
-                    shuffleBoxs[i].gameObject.SetActive(true);
-                }
-                else
-                {
-                    shuffleBoxs[i].DestroyBoxInfo();
-                    shuffleBoxs[i].gameObject.SetActive(false);
-                }
-            }
+            return Mathf.Clamp(StageManager.instance.difficulty - 1, 0, obstacles.Length - 1);
         }
+        else
+            return 0;
     }
 }
