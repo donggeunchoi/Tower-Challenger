1a1a1bdc74f868a5dc52fc5fae36757a07b9d40b
donggeunchoi <cdg01063149045@gmail.com>
2025-08-07 21:40:59 +0900

    Update : 마리아NPC 움직임 구현완료

--- Assets/03 Scripts/Village/NPC/NPC4Controller.cs
+++ Assets/03 Scripts/Village/NPC/NPC4Controller.cs
@@ -2,97 +2,30 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class NPC4Controller : MonoBehaviour
+public class NPC4Controller : NPCBase
 {
-    public NPCData npcData;
-    public GameObject talkImage;
-    public TMPro.TMP_Text talkText;
-    private Animator animator;
-    
-    private float _startPositionX;
-    private bool _movingLeft = true;
-    private bool _isWaiting = false;
+   protected override IEnumerator WaitBeforeTurn(bool turnToRight)
+   {
+      _isWaiting = true;
+      _animator.SetBool("IsMove", false);
 
-    void Start()
-    {
-        _startPositionX = transform.localPosition.x;
-        animator = GetComponent<Animator>();
-    }
+      if (!turnToRight)
+      {
+         talkImage.SetActive(true);
+         RandomDescription();
+      }
+      else
+      {
+         talkImage.SetActive(false);
+         // 여기서는 이미지 비활성화 안함
+      }
 
-    void Update()
-    {
-        //나중에 여기에 대사 집어넣기.
-        if (_isWaiting)
-        {
-            return;
-        }
-        
-        animator.SetBool("IsMove", true);
-        
-        float newX = transform.localPosition.x;
+      yield return new WaitForSeconds(npcData.StopDuration);
 
-        if (_movingLeft)
-        {
-            newX -= npcData.MoveSpeed*Time.deltaTime;
-            if (newX <= _startPositionX - npcData.MoveDistance)
-            {
-                _movingLeft = false;
-                StartCoroutine(WaitBeforeTurn(false));
-            }
-        }
-        else
-        {
-            newX += npcData.MoveSpeed*Time.deltaTime;
-            if (newX >= _startPositionX + npcData.MoveDistance)
-            {
-                _movingLeft = true;
-                StartCoroutine(WaitBeforeTurn(true));
-            }
-        }
-        transform.localPosition = new Vector3(newX, transform.localPosition.y, transform.localPosition.z);
-    }
-
-    private void Flip(bool faceLeft)
-    {
-        Vector3 scale = transform.localScale;
-        scale.x = faceLeft ? -1 : 1;
-        transform.localScale = scale;
-    }
-
-    private IEnumerator WaitBeforeTurn(bool turnToRight)
-    {
-        _isWaiting = true;
-        
-        animator.SetBool("IsMove", false);
-
-        if (!turnToRight)
-        {
-            talkImage.SetActive(true);
-            RandomDescription();
-        }
-        else
-        {
-            talkImage.SetActive(false);
-            // GetComponent<Image>().enabled = false;
-        }
-        
-        yield return new WaitForSeconds(npcData.StopDuration);
-        
-        // GetComponent<Image>().enabled = true;
-        _movingLeft = turnToRight;
-        Flip(!turnToRight);
-        _isWaiting = false;
-        talkImage.SetActive(false);
-    }
-    
-    private void RandomDescription()
-    {
-        if (npcData.npcDescription != null && npcData.npcDescription.Length > 0)
-        {
-            int randomIndex = Random.Range(0, npcData.npcDescription.Length);
-            talkText.text = npcData.npcDescription[randomIndex];
-            talkImage.SetActive(true);
-        }
-    }
+      _movingLeft = turnToRight;
+      Flip(!turnToRight);
+      _isWaiting = false;
+      talkImage.SetActive(false);
+   }
 }
 
