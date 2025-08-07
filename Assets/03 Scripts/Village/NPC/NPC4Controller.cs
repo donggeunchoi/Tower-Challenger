5bcf0fa6a266e80adf843e7fe775654c08840434
donggeunchoi <cdg01063149045@gmail.com>
2025-08-07 12:22:43 +0900

    Update : NPC4 구현

---
+++ Assets/03 Scripts/Village/NPC/NPC4Controller.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC4Controller : MonoBehaviour
+{
+    public NPCData npcData;
+    public GameObject talkImage;
+    public TMPro.TMP_Text talkText;
+    private Animator animator;
+    
+    private float _startPositionX;
+    private bool _movingLeft = true;
+    private bool _isWaiting = false;
+
+    void Start()
+    {
+        _startPositionX = transform.localPosition.x;
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //나중에 여기에 대사 집어넣기.
+        if (_isWaiting)
+        {
+            return;
+        }
+        
+        animator.SetBool("IsMove", true);
+        
+        float newX = transform.localPosition.x;
+
+        if (_movingLeft)
+        {
+            newX -= npcData.MoveSpeed*Time.deltaTime;
+            if (newX <= _startPositionX - npcData.MoveDistance)
+            {
+                _movingLeft = false;
+                StartCoroutine(WaitBeforeTurn(false));
+            }
+        }
+        else
+        {
+            newX += npcData.MoveSpeed*Time.deltaTime;
+            if (newX >= _startPositionX + npcData.MoveDistance)
+            {
+                _movingLeft = true;
+                StartCoroutine(WaitBeforeTurn(true));
+            }
+        }
+        transform.localPosition = new Vector3(newX, transform.localPosition.y, transform.localPosition.z);
+    }
+
+    private void Flip(bool faceLeft)
+    {
+        Vector3 scale = transform.localScale;
+        scale.x = faceLeft ? -1 : 1;
+        transform.localScale = scale;
+    }
+
+    private IEnumerator WaitBeforeTurn(bool turnToRight)
+    {
+        _isWaiting = true;
+        
+        animator.SetBool("IsMove", false);
+
+        if (!turnToRight)
+        {
+            talkImage.SetActive(true);
+            RandomDescription();
+        }
+        else
+        {
+            talkImage.SetActive(false);
+            // GetComponent<Image>().enabled = false;
+        }
+        
+        yield return new WaitForSeconds(npcData.StopDuration);
+        
+        // GetComponent<Image>().enabled = true;
+        _movingLeft = turnToRight;
+        Flip(!turnToRight);
+        _isWaiting = false;
+        talkImage.SetActive(false);
+    }
+    
+    private void RandomDescription()
+    {
+        if (npcData.npcDescription != null && npcData.npcDescription.Length > 0)
+        {
+            int randomIndex = Random.Range(0, npcData.npcDescription.Length);
+            talkText.text = npcData.npcDescription[randomIndex];
+            talkImage.SetActive(true);
+        }
+    }
+}
+
